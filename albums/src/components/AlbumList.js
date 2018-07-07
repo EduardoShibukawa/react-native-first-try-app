d4176bf509fb776232587e55282586664fbd5a75
unknown <eduardoshibuka@gmail.com>
2018-07-06 22:59:56 -0300

    Mostrando albums na tela

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {    
+    state = { albums: [] };
 
     componentWillMount() {
         fetch('https://rallycoding.herokuapp.com/api/music_albums')
-            .then((response) => response.json());        
+            .then(response => response.json())
+            .then(json => this.setState({ albums: json }));        
+    }
+
+    renderAlbums() {   
+        return this.state.albums.map(album => <Text>{album.title}</Text>);                     
     }
 
     render() {
         return (
             <View>
-                <Text> Album List!! </Text>
+              {this.renderAlbums()}
             </View>
-        );
+      );
     }
 }
 
